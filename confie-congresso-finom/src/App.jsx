44060538a75a0d826489516b1d904f05b1bfc197
Leandro Mateus <leandroyow@gmail.com>
2023-09-28 21:57:48 -0300

    bootstrap and base added

---
+++ confie-congresso-finom/src/App.jsx
@@ -0,0 +1,18 @@
+import './styles/App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <>    
+          <nav className='nav'>
+            <img src="./assets/LOGO.png" alt="CONFIE LOGO" />
+          </nav>
+          <div></div>
+          <footer>Finom 2023</footer>
+    </>
+  )
+}
+
+export default App
