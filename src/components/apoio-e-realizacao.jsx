7930545c2529ab66d94d1418fec5342587f46618
Leandro Mateus <leandroyow@gmail.com>
2023-10-02 19:03:18 -0300

    logos desesticadose mais espaçamento entre eles

--- src/components/apoio-e-realizacao.jsx
+++ src/components/apoio-e-realizacao.jsx
@@ -4,11 +4,17 @@ export function Apoio_e_Realizacao() {
     <div>
       <h3 className="p-3 titulo-patrocina fw-bold">Apoio</h3>
       <div className="p-3 apoio-realizacao-img-container">
-        <img src="/assets/sebrae.webp" alt="SEBRAE Logo" />
+        <img 
+        src="/assets/sebrae-logo-0.png" 
+        alt="SEBRAE Logo" 
+        id="sebrae-logo-cima"
+        style={{height: 150, width: 220}}
+        />
         <img
-          style={{ height: 75, width: 130 }}
+          style={{ height: 150, width: 220 }}
           src="/assets/fiemg.webp"
           alt="FIEMG Logo"
+          id="fiemg-logo-cima"
         />
       </div>
     </div>
@@ -17,13 +23,13 @@ export function Apoio_e_Realizacao() {
       <h3 className="p-3 titulo-patrocina fw-bold">Realização</h3>
       <div className="p-3 apoio-realizacao-img-container">
         <img
-          style={{ height: 75, width: 190 }}
+          style={{ height: 75, width: 280 }}
           src="/assets/finom-branco.png"
           alt="Finom"
           id="finom-logo-cima"
         />
         <img
-          style={{ height: 80, width: 140 }}
+          style={{ height: 150, width: 250 }}
           src="/assets/fonseca-branco.png"
           alt="Colégio Fonseca Rodrigues"
           id="colegio-logo-cima"
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,11 +5,15 @@ export function Footer() {
       <div className="d-flex justify-content-center pt-5">
         <div className="line text-center"> </div>
       </div>
-    <div className="patrocinadores">
+    <div className="patrocinadores gap-3">
       <h6 className="text-center fs-4 pt-3 fw-bold">Patrocinadores</h6>
-      <div className="d-flex justify-content-around">
+      <div className="d-flex justify-content-around gap-3">
         <img src="/assets/sicoob.png" alt="Sicoob" />
-        <img src="/assets/fiemg.webp" alt="FIEMG" />
+        <img 
+        src="/assets/fiemg.webp" 
+        alt="FIEMG"
+        style={{height: 110}}
+        />
       </div>
     </div>
 
@@ -35,30 +39,30 @@ export function Footer() {
     <h3 className="fw-bold text-white fs-6 mb-3">Apoio</h3>
     <div className="apoio-realizacao-img-container mb-5">
   <img 
-  src="/assets/sebrae.webp"
+  src="/assets/sebrae-logo-0.png"
    alt="SEBRAE Logo"
-   style={{height: 60}}
+   style={{height: 85, width: 180}}
     />
   <img
-    style={{ height: 60, width: 110 }}
+    style={{ height: 85, width: 180 }}
     src="/assets/fiemg.webp"
     alt="FIEMG Logo"
   />
 </div>
-<div>
+<div className="pb-5">
       <h3 className="fw-bold text-white fs-6" id="texto-realizacao">Realização</h3>
       <div className="apoio-realizacao-img-container pb-1">
         <img
           style={{ height: 60, width: 160 }}
           src="/assets/finom-branco.png"
           alt="Finom Logo"
-          id="logo-finom"
+          id="logo-finom-footer"
         />
         <img
-          style={{ height: 80, width: 140 }}
+          style={{ height: 150, width: 140 }}
           src="/assets/fonseca-branco.png"
           alt="Colégio Fonseca Rodrigues"
-          id="logo-fonseca"
+          id="logo-fonseca-footer"
         />
       </div>
     </div>
