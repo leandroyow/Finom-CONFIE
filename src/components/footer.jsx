ea4917557a3d288ecf5160298f5e316dcf4bc839
Leandro Mateus <leandroyow@gmail.com>
2023-10-05 14:16:58 -0300

    Componente dos palestrantes criado

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,223 +1,295 @@
 import { useMediaQuery } from "react-responsive";
 import { useEffect, useRef } from "react";
-import { useSwiper } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 export function Footer() {
   const isLessThan637 = useMediaQuery({ query: "(max-width: 637px)" });
   const isLessThan436 = useMediaQuery({ query: "(max-width: 436px)" });
-  
-  const swipe = useSwiper();
+
   const swiperRef = useRef(null);
-  console.log(swipe)
+
+  const navigationPrevRef = useRef(null)
+  const navigationNextRef = useRef(null)
   
   useEffect(() => {
+
     const swiperContainer = swiperRef.current;
-    
-    const params = {
-      
-      navigation: [{
-        nextEl: "assets/arrow-right-circle.svg",
-        prevEl: "assets/arrow-left-circle.svg",
-        enabledClass: "swiper-button-active"
-      }],
-      injectStyles: [
-        `
-        .swiper-button-next::after,
-        .swiper-button-prev::after {
-          content: "" !important;
-        }
-          .swiper-button-next {
-            position: absolute;
-          }
-          .swiper-button-prev {
-            background-color: transparent;
-            background-position: center;
-            background-size: 40px;
-            background-repeat: no-repeat;
-            padding: 16px 16px;
-            border-radius: 100%;
-            border: 2px solid black;
-            color: white;
-          }
-
-          .swiper-button-prev  {
-            background-image: url("assets/arrow-left-circle.svg") !important;
-          }
-
-          .swiper-button-next {
-            background-image: url("assets/arrow-right-circle.svg") !important;
-          }
-
-      `,
-      ],
-    };
-
-    Object.assign(swiperContainer, params);
+    Object.assignswiperContainer;
     swiperContainer.initialize();
+    
+    
   }, []);
 
   return (
     <>
-      
       <h5 className="titulo-palestrantes fs-4 text-center mt-5 mb-4">
         Palestrantes
       </h5>
-      
-      <div className="container-swipes">
+      <div>
+
+      <swiper-container ref={swiperRef}  >
         
-        <swiper-container init="false" ref={swiperRef}>
-          <swiper-slide>
-            <div className="card">
-              <div className="d-flex">
-                <div className="img-container">
-                  <img
-                    style={{ height: 144, width: 144 }}
-                    src="assets/palestrante (1).jpeg"
-                    className="card-img rounded-circle"
-                    alt="Maykell Pereira"
-                  />
-                </div>
-                <div style={{ width: 280, padding: 0 }} className="">
-                  <div className="card-body d-flex flex-column justify-content-between">
-                    <h5 className="card-title">Maykell Pereira</h5>
-                    <div className="card-text-scroll">
-                    <p className="card-text">
-                      Diretor de Operações de TI. Auma Tecnologia
-                    </p>
+          <Swiper 
+          modules={[Navigation]}
+          navigation={{
+            prevEl: navigationPrevRef.current,
+            nextEl: navigationNextRef.current,
+          }}
+         onBeforeInit={(swiper) => {
+              swiper.params.navigation.prevEl = navigationPrevRef.current;
+              swiper.params.navigation.nextEl = navigationNextRef.current;
+         }}>
+
+            <swiper-wrapper>
+
+
+            <SwiperSlide>
+              <div className="card">
+                <div className="d-flex">
+                  <div className="img-container">
+                    <img
+                      style={{ height: 144, width: 144 }}
+                      src="assets/palestrante (1).jpeg"
+                      className="card-img rounded-circle"
+                      alt="Maykell Pereira"
+                    />
+                  </div>
+                  <div style={{ width: 280, padding: 0 }} className="">
+                    <div className="card-body d-flex flex-column justify-content-between">
+                      <h5 className="card-title">Maykell Pereira</h5>
+                      <div className="card-text-scroll">
+                        <p className="card-text">
+                          Diretor de Operações de TI. Auma Tecnologia
+                        </p>
+                      </div>
+                      <button className="btn btn-outline-dark">
+                        Conheça-me
+                      </button>
                     </div>
-                    <button className="btn btn-outline-dark">Conheça-me</button>
                   </div>
                 </div>
               </div>
-            </div>
-          </swiper-slide>
-          <swiper-slide>
-            <div className="card">
-              <div className="d-flex">
-                <div className="img-container">
-                  <img
-                    style={{ height: 144, width: 144 }}
-                    src="assets/palestrante (2).jpeg"
-                    className="card-img rounded-circle"
-                    alt="Carlos Simão"
-                  />
-                </div>
-                <div style={{ width: 280, padding: 0 }} className="">
-                  <div className="card-body d-flex flex-column justify-content-between">
-                    <h5 className="card-title">Carlos Simão</h5>
-                    <p className="card-text">
-                    Arquiteto de Soluções. Auma Tecnologia
-                    </p>
-                    <button className="btn btn-outline-dark">Conheça-me</button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="card">
+                <div className="d-flex">
+                  <div className="img-container">
+                    <img
+                      style={{ height: 144, width: 144 }}
+                      src="assets/palestrante (2).jpeg"
+                      className="card-img rounded-circle"
+                      alt="Carlos Simão"
+                    />
+                  </div>
+                  <div style={{ width: 280, padding: 0 }} className="">
+                    <div className="card-body d-flex flex-column justify-content-between">
+                      <h5 className="card-title">Carlos Simão</h5>
+                      <p className="card-text">
+                      Team Leader na Auma Tecnologia
+                      </p>
+                      <button className="btn btn-outline-dark">
+                        Conheça-me
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </swiper-slide>
-          <swiper-slide>
-            <div className="card">
-              <div className="d-flex">
-                <div className="img-container">
-                  <img
-                    style={{ height: 144, width: 144 }}
-                    src="assets/palestrante (3).jpeg"
-                    className="card-img rounded-circle"
-                    alt="Rayanne Bárbara Pereira"
-                  />
-                </div>
-                <div style={{ width: 280, padding: 0 }} className="">
-                  <div className="card-body d-flex flex-column justify-content-between">
-                    <h5 className="card-title">Rayanne Pereira</h5>
-                    <p className="card-text">
-                    Coordenadora da Área de Assessoria Estratégica e Soluções Judiciais Cíveis e Trabalhistas, escritório Mírian Gontijo Advogados
-                    </p>
-                    <button className="btn btn-outline-dark">Conheça-me</button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="card">
+                <div className="d-flex">
+                  <div className="img-container">
+                    <img
+                      style={{ height: 144, width: 144 }}
+                      src="assets/palestrante (3).jpeg"
+                      className="card-img rounded-circle"
+                      alt="Rayanne Bárbara Pereira"
+                    />
+                  </div>
+                  <div style={{ width: 280, padding: 0 }} className="">
+                    <div className="card-body d-flex flex-column justify-content-between">
+                      <h5 className="card-title">Rayanne Pereira</h5>
+                      <p className="card-text">
+                        Coordenadora da Área de Assessoria Estratégica e
+                        Soluções Judiciais Cíveis e Trabalhistas, escritório
+                        Mírian Gontijo Advogados
+                      </p>
+                      <button className="btn btn-outline-dark">
+                        Conheça-me
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </swiper-slide>
-          <swiper-slide>
-            <div className="card">
-              <div className="d-flex">
-                <div className="img-container">
-                  <img
-                    style={{ height: 144, width: 144 }}
-                    src="assets/palestrante (4).jpeg"
-                    className="card-img rounded-circle"
-                    alt="Igor Tadeu Fernandes Rodrigues"
-                  />
-                </div>
-                <div style={{ width: 280, padding: 0 }} className="">
-                  <div className="card-body d-flex flex-column justify-content-between">
-                    <h5 className="card-title">Igor Rodrigues</h5>
-                    <p className="card-text">
-                    Analista do Ministério Público do Estado de Minas Gerais
-                    </p>
-                    <button className="btn btn-outline-dark">Conheça-me</button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="card">
+                <div className="d-flex">
+                  <div className="img-container">
+                    <img
+                      style={{ height: 144, width: 144 }}
+                      src="assets/palestrante (4).jpeg"
+                      className="card-img rounded-circle"
+                      alt="Igor Tadeu Fernandes Rodrigues"
+                    />
+                  </div>
+                  <div style={{ width: 280, padding: 0 }} className="">
+                    <div className="card-body d-flex flex-column justify-content-between">
+                      <h5 className="card-title">Igor Rodrigues</h5>
+                      <p className="card-text">
+                        Analista do Ministério Público do Estado de Minas Gerais
+                      </p>
+                      <button className="btn btn-outline-dark">
+                        Conheça-me
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </swiper-slide>
-          <swiper-slide>
-            <div className="card">
-              <div className="d-flex">
-                <div className="img-container">
-                  <img
-                    style={{ height: 144, width: 144 }}
-                    src="assets/palestrante (5).jpeg"
-                    className="card-img rounded-circle"
-                    alt="Adriano Chaves Macedo"
-                  />
-                </div>
-                <div style={{ width: 280, padding: 0 }} className="">
-                  <div className="card-body d-flex flex-column justify-content-between">
-                    <h5 className="card-title">Adriano Macedo</h5>
-                    <p className="card-text">
-                    Analista de Planejamento e Controle de Manutenção, KWS Group
-                    </p>
-                    <button className="btn btn-outline-dark">Conheça-me</button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="card">
+                <div className="d-flex">
+                  <div className="img-container">
+                    <img
+                      style={{ height: 144, width: 144 }}
+                      src="assets/palestrante (5).jpeg"
+                      className="card-img rounded-circle"
+                      alt="Adriano Chaves Macedo"
+                    />
+                  </div>
+                  <div style={{ width: 280, padding: 0 }} className="">
+                    <div className="card-body d-flex flex-column justify-content-between">
+                      <h5 className="card-title">Adriano Macedo</h5>
+                      <p className="card-text">
+                        Analista de Planejamento e Controle de Manutenção, KWS
+                        Group
+                      </p>
+                      <button className="btn btn-outline-dark">
+                        Conheça-me
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </swiper-slide>
-          <swiper-slide>
-            <div className="card">
-              <div className="d-flex">
-                <div className="img-container">
-                  <img
-                    style={{ height: 144, width: 144 }}
-                    src="assets/palestrante (6).jpeg"
-                    className="card-img rounded-circle"
-                    alt="Higgor Olipe"
-                  />
-                </div>
-                <div style={{ width: 280, padding: 0 }} className="">
-                  <div className="card-body d-flex flex-column justify-content-between">
-                    <h5 className="card-title">Higgor Olipe</h5>
-                    <p className="card-text">
-                    Gerente de Automação e Digital. Grupo Setta
-                    </p>
-                    <button className="btn btn-outline-dark">Conheça-me</button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="card">
+                <div className="d-flex">
+                  <div className="img-container">
+                    <img
+                      style={{ height: 144, width: 144 }}
+                      src="assets/palestrante (8).jpeg"
+                      className="card-img rounded-circle"
+                      alt="Nívia Mendes Gontijo"
+                    />
+                  </div>
+                  <div style={{ width: 280, padding: 0 }} className="">
+                    <div className="card-body d-flex flex-column justify-content-between">
+                      <h5 className="card-title">Nívia Mendes Gontijo</h5>
+                      <p className="card-text">
+                      Escrivã Judicial, TJMG
+                      </p>
+                      <button className="btn btn-outline-dark">
+                        Conheça-me
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </swiper-slide>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="card">
+                <div className="d-flex">
+                  <div className="img-container">
+                    <img
+                      style={{ height: 144, width: 144 }}
+                      src="assets/palestrante (7).jpeg"
+                      className="card-img rounded-circle"
+                      alt="Willian Ferreira"
+                    />
+                  </div>
+                  <div style={{ width: 280, padding: 0 }} className="">
+                    <div className="card-body d-flex flex-column justify-content-between">
+                      <h5 className="card-title">Willian Ferreira</h5>
+                      <p className="card-text">
+                        Assessor Pedagógico, Grupo Bernoulli de Educação
+                      </p>
+                      <button className="btn btn-outline-dark">
+                        Conheça-me
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="card">
+                <div className="d-flex">
+                  <div className="img-container">
+                    <img
+                      style={{ height: 144, width: 144 }}
+                      src="assets/palestrante (9).jpeg"
+                      className="card-img rounded-circle"
+                      alt="Andressa Arielly"
+                    />
+                  </div>
+                  <div style={{ width: 280, padding: 0 }} className="">
+                    <div className="card-body d-flex flex-column justify-content-between">
+                      <h5 className="card-title">Andressa Arielly</h5>
+                      <p className="card-text">
+                      Especialista em planejamento de carreira
+                      </p>
+                      <button className="btn btn-outline-dark">
+                        Conheça-me
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="card">
+                <div className="d-flex">
+                  <div className="img-container">
+                    <img
+                      style={{ height: 144, width: 144 }}
+                      src="assets/palestrante (10).jpeg"
+                      className="card-img rounded-circle"
+                      alt="Weber Ferreira"
+                    />
+                  </div>
+                  <div style={{ width: 280, padding: 0 }} className="">
+                    <div className="card-body d-flex flex-column justify-content-between">
+                      <h5 className="card-title">Weber Ferreira</h5>
+                      <p className="card-text">
+                      CEO, Construtora Ferreira
+                      </p>
+                      <button className="btn btn-outline-dark">
+                        Conheça-me
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            </swiper-wrapper>
+          </Swiper >
+         
         </swiper-container>
+        <div className="d-flex justify-content-center gap-3 pt-3">
+          <i className="fa-solid fa-circle-arrow-left"
+             role="button"
+             ref={navigationPrevRef} 
+             ></i>
+          <i className="fa-solid fa-circle-arrow-right"
+             role="button"
+             ref={navigationNextRef} ></i>
+         </div>
       </div>
-
-      <div className="container-navigation-btn text-center">
-        <button role="button" >
-      <img src="assets/arrow-left-circle.svg" alt="" />
-        </button>
-        <button role="button" onClick={() => swipe.slideNext()}>
-      <img src="assets/arrow-right-circle.svg" alt="" />
-        </button>
-      </div>
-
 
       <div className="patrocinadores gap-3">
         <h6 className="text-center fs-4 pt-3 fw-bold">Patrocinadores</h6>
